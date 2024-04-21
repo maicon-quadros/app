ddd432adb2b597282a51321278df324fba72888e
maicon-quadros <maiconw.quadros@gmail.com>
2024-04-21 20:32:23 -0300

    first-commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="px-3 py-10 md:px-10">
+        <div class="w-full sm:w-1/2 lg:w-1/3 mx-auto">
+
+            <TodoSpinner v-if="loading" />
+            <template v-else>
+                <TodoFormAdd />
+
+                <TodoItems v-if="$store.state.todos.length" />
+
+                <TodoEmpty v-else />
+            </template>
+
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoSpinner from '@/components/TodoSpinner';
+import TodoFormAdd from '@/components/TodoFormAdd.vue';
+import TodoItems from '@/components/TodoItems.vue';
+import TodoEmpty from '@/components/TodoEmpty';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+    components: { TodoSpinner, TodoFormAdd, TodoItems,TodoEmpty },
+
+    data() {
+        return {
+            loading: false
+        }
+    },
+       
+    created() {
+        this.loading = true
+        this.$store.dispatch('getTodos').finally(() => {
+            this.loading = false
+        })
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
